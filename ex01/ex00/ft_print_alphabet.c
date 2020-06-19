01a5728474a73c423ef32e70d2f5eaa3cab1f19e
Pfumelo Shikweni <pshikwen@c4r13s8.wethinkcode.co.za>
2020-06-19 12:12:47 +0200

    done

---
+++ ex01/ex00/ft_print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int ch = 97;
+
+    printf("Alphabets from a - z are: \n");
+    while(ch<=122)
+    {
+        printf("%c\n", ch);
+        ch++;
+    }
+
+    return 0;
+}
